553589d65b8bb281314c829751f5aec480fa20e0
Carlos Fernández San Millán <cfernand1977@gmail.com>
2016-12-26 13:58:16 +0100

    Code simplification and reduction.

--- main.py
+++ main.py
@@ -10,53 +10,20 @@ class Main:
     def random_generator(self):
         import random
 
-        elems = 1000000
+        elems = 10000
 
         random_items = [random.randint(1, elems) for num_items in range(elems)]
         return random_items
+
 
 main = Main()
 sort = sorting_algorithms.Sorting_Algorithms()
 
 random_items = main.random_generator()
+function_list = [sort.bucket_sort, sort.bubble_sort, sort.insertion_sort, sort.merge_sort, sort.shell_sort, sort.quick_sort, sort.radix_sort, sort.heap_sort]
+functions_names = ["Bucket Sort", "Bubble Sort", "Insertion Sort", "Merge Sort", "Shell Sort", "Quick Sort", "Radix Sort", "Heap Sort"]
 
-items = main.random_generator()
-start_time = time.time()
-sort.bucket_sort(items)
-print("--- seconds for Bucket Sort:    %s " % (time.time() - start_time))
-
-# items = random_items[:]
-# start_time = time.time()
-# sort.bubble_sort(items)
-# print("--- seconds for Bubble Sort:    %s " % (time.time() - start_time))
-
-# items = random_items[:]
-# start_time = time.time()
-# sort.insertion_sort(items)
-# print("--- seconds for Insertion Sort: %s " % (time.time() - start_time))
-
-items = random_items[:]
-start_time = time.time()
-sort.merge_sort(items)
-print("--- seconds for Merge Sort:     %s " % (time.time() - start_time))
-
-items = random_items[:]
-start_time = time.time()
-sort.shell_sort(items)
-print("--- seconds for Shell Sort:     %s " % (time.time() - start_time))
-
-items = random_items[:]
-start_time = time.time()
-sort.quick_sort(items)
-print("--- seconds for Quick Sort:     %s " % (time.time() - start_time))
-
-items = main.random_generator()
-start_time = time.time()
-sort.radix_sort(items)
-print("--- seconds for Radix Sort:     %s " % (time.time() - start_time))
-
-items = random_items[:]
-start_time = time.time()
-sort.heap_sort(items)
-print("--- seconds for Heap Sort:      %s " % (time.time() - start_time))
-
+for functions in range(0, len(function_list)):
+    start_time = time.time()
+    function_list[functions](random_items[:])
+    print("--- seconds for "+functions_names[functions]+" :    %s " % (time.time() - start_time))
